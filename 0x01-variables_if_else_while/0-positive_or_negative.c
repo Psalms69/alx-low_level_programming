31a1e1a7a263d40de8828f6d5a37223463c14dd1
Eniafe dara Emmanuel <donloius0@gmail.com>
2022-06-03 06:44:17 -0700

    all

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -13,13 +13,13 @@ n = rand() - RAND_MAX / 2;
 if (n <= 0)
 {
 if (n == 0)
-printf("%d is zero", n);
+printf(" %d is zero", n);
 else
-printf("%d is negative", n);
+printf(" %d is negative", n);
 }
 else
 {
-printf("%d is positive", n);
+printf(" %d is positive", n);
 }
 return (0);
 }
