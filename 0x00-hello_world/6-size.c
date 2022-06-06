106fbb2ddbaf02ac3d9e788c7d868d26aaef816c
Eniafe dara Emmanuel <donloius0@gmail.com>
2022-06-06 04:15:42 -0700

    task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,4 +1,3 @@
-#!/bin/bash
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -16,4 +15,5 @@ int main(void)
 	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(b));
 	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(c));
 	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
-	return (0);}
+	return (0);
+}
