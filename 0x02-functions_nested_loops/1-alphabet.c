a021153a3fae0a2575c6b0feddb0e13c17d28a6c
Eniafe dara Emmanuel <donloius0@gmail.com>
2022-07-13 09:09:06 -0700

    func print a-z

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,10 +1,16 @@
 #include "main.h"
-void print_alphabet(void);
+
+/**
+ * print_alphabet - Prints the alphabet in lowercase.
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet(void)
 {
-char ch 
-for(ch = '67';ch <='122'; ch++)
-{
-_putchar(ch);
-}
-_putchar("/n");
+	int ch = 'a';
+	while (ch <= 'z')
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
 }
