df9e4cbf1ebbc0f00ac8d246320ccb08db553bab
Eniafe dara Emmanuel <donloius0@gmail.com>
2022-07-13 09:38:31 -0700

    2done

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,3 +1,6 @@
+#ifndef MAIN_H_INCLUDED
+#define MAIN_H_INCLUDED
+
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
@@ -8,4 +11,8 @@ void jack_bauer(void);
 void times_table(void);
 int add(int, int);
 void print_to_98(int n);
+
+
+
+#endif // MAIN_H_INCLUDED
 
