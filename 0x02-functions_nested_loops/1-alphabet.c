66b8e160fb4d1d46c2db158cdedab7dfa3289294
Eniafe dara Emmanuel <donloius0@gmail.com>
2022-07-13 09:13:43 -0700

    done

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,7 +2,8 @@
 
 /**
  * print_alphabet - Prints the alphabet in lowercase.
- *
+ * Description: a function that prints the alphabet,
+ * in lowecase followed by a new line
  * Return: Always 0 (Success)
  */
 void print_alphabet(void)
@@ -10,7 +11,7 @@ void print_alphabet(void)
 	int ch = 'a';
 	while (ch <= 'z')
 	{
-		_putchar(i);
+		_putchar(ch);
 	}
 	_putchar('\n');
 }
